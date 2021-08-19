633831f622a1158c818caf1a830d86a007dea0a8
Glenn Gunter <gunterglenn33@gmail.com>
2021-08-19 17:23:47 +0200

    added style and show page, api working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,12 +2,17 @@ require 'json'
 require 'open-uri'
 
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home, :recipies ]
+  skip_before_action :authenticate_user!, only: [ :home, :recipies, :show ]
 
   def home
   end
 
   def recipies
+    recipies = search(params[:search])
+    @recipies = recipies["results"]
+  end
+
+  def show
   end
 
   private
@@ -15,6 +20,6 @@ class PagesController < ApplicationController
   def search(word)
     url = "https://api.spoonacular.com/recipes/complexSearch?query=#{word}&apiKey=76b2170d78fc4e8b836a1055b10502a7"
     serialized_url = URI.open(url).read
-    p recipie = JSON.parse(serialized_url)
+    recipie = JSON.parse(serialized_url)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   get 'recipies', to: 'pages#recipies'
-  resources :bookings, only: [:index]
+  get 'recipies/:id', to: 'pages#show', as: :recipie
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
