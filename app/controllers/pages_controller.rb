bbe539d05b1de63b1626bcd0d73381f0e761660e
Glenn Gunter <gunterglenn33@gmail.com>
2021-08-19 16:44:00 +0200

    added api

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,3 +1,6 @@
+require 'json'
+require 'open-uri'
+
 class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [ :home, :recipies ]
 
@@ -5,5 +8,13 @@ class PagesController < ApplicationController
   end
 
   def recipies
+  end
+
+  private
+
+  def search(word)
+    url = "https://api.spoonacular.com/recipes/complexSearch?query=#{word}&apiKey=76b2170d78fc4e8b836a1055b10502a7"
+    serialized_url = URI.open(url).read
+    p recipie = JSON.parse(serialized_url)
   end
 end
